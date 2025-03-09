6dad9a986efb9a88a48faaf472393e59d29981b2
Crash <crasevschiserghei@gmail.com>
2025-03-10 00:12:15 +0100

    appCRUD

---
+++ my-app/src/components/app-info/app-info.js
@@ -0,0 +1,1 @@
+import './app-info.css';const AppInfo = () => {  return (    <div className="app-info">      <h1>Учет сотрудников в компании N</h1>      <h2>Обшее число сотрудников: </h2>      <h2>Премию получит: </h2>    </div>  )}export default AppInfo;
