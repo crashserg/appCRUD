d8127032cb11770bbdc53ac15426fba6afaba2ae
Crash <crasevschiserghei@gmail.com>
2025-03-13 00:36:42 +0100

    fix bugs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/app/app';
+import { createRoot } from "react-dom/client";
+import App from "./components/app/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
